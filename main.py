4845d843a0dd2c5a8ddd5bacd6eebdb66d01b726
Shah Fazle Elahi <shahfazleelahi@Shah-Fazle-Elahi.local>
2024-08-01 07:24:07 +0600

    How To Comme nt single line or multi line

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Shah Fazle Elahi Shaki")
+#Singel Line Comment just use "#" and type anything paython will ignore it.
+
+'''if you want to comment multiline of line for the comment you have to use
+"singel cotetion" like i type and paython will ignore it'''
+
+print("Hello Shah Fazle Elahi Shaki")
 
